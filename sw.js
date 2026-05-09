68e7659e4ee7c98261f9f86a5b6478a18162d855
Moshe Galam <moshegalam@gmail.com>
2026-05-09 21:43:37 +0300

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "alum-cache-v2";
+const CACHE_NAME = "alum-cache-v5";
 
 const urlsToCache = [
   "/",
@@ -7,17 +7,33 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
   );
 });
